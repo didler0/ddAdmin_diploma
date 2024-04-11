68a79d4c56314435ea2874da511c8b111f4282be
didler <rwxzznbv@gmail.com>
2024-04-11 17:39:48 +0300

    finished aioBranchOfficeStructuralUnit.py

--- aioBranchOfficeStructuralUnit.py
+++ aioBranchOfficeStructuralUnit.py
@@ -71,53 +71,58 @@ class AioBranchOfficeStructuralUnit(customtkinter.CTkToplevel):
         add_new_structural_units_to_branch_button.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
 
     def add_new_structural_units_to_branch(self):
-        list_of_str_units = []
-        branch_office = self.branch_office_combob.get_current_value()
-
-        # Получение списка введенных структурных подразделений
-        for widget in self.list_widgets_only_entry:
-            if isinstance(widget, customtkinter.CTkEntry) and widget.get() != '':
-                list_of_str_units.append(widget.get())
-        print(branch_office)
-        print(list_of_str_units)
-
         try:
-            # Получение идентификатора филиала
-            id_branch = db_manager.get_data("branch_office", "id", f"name = '{branch_office}'")[0][0]
-            print(id_branch)
-        except IndexError:
-            CTkMessagebox(title="Ошибка", message="Выбранный филиал не найден в базе данных.", icon="cancel")
-            return
+            list_of_str_units = []
+            branch_office = self.branch_office_combob.get_current_value()
 
-        # Проверка и добавление отсутствующих структурных подразделений
-        try:
-            for text in list_of_str_units:
-                # Проверяем, существует ли структурное подразделение в базе данных
-                if not db_manager.check_structural_unit_exists(text):
-                    # Если структурное подразделение отсутствует, добавляем его
-                    db_manager.insert_data("structural_unit", "name", f"'{text}'")
+            # Получение списка введенных структурных подразделений
+            for widget in self.list_widgets_only_entry:
+                if isinstance(widget, customtkinter.CTkEntry) and widget.get() != '':
+                    list_of_str_units.append(widget.get())
+            print(branch_office)
+            print(list_of_str_units)
+
+            try:
+                # Получение идентификатора филиала
+                id_branch = db_manager.get_data("branch_office", "id", f"name = '{branch_office}'")[0][0]
+                print(id_branch)
+            except IndexError:
+                CTkMessagebox(title="Ошибка", message="Выбранный филиал не найден в базе данных.", icon="cancel")
+                return
+
+            # Проверка и добавление отсутствующих структурных подразделений
+            try:
+                for text in list_of_str_units:
+                    # Проверяем, существует ли структурное подразделение в базе данных
+                    if not db_manager.check_structural_unit_exists(text):
+                        # Если структурное подразделение отсутствует, добавляем его
+                        db_manager.insert_data("structural_unit", "name", f"'{text}'")
+            except Exception as e:
+                CTkMessagebox(title="Ошибка", message=f"Ошибка при добавлении структурного подразделения: {str(e)}", icon="cancel")
+                return
+
+            # Получение идентификаторов структурных подразделений и их добавление к филиалу
+            try:
+                for text in list_of_str_units:
+                    # Проверяем, привязано ли структурное подразделение к филиалу
+                    if not db_manager.check_structural_unit_assigned_to_branch(text, branch_office):
+                        # Если структурное подразделение не привязано к филиалу, добавляем его привязку
+                        structural_unit_id = db_manager.get_data("structural_unit", "id", f"name = '{text}'")[0][0]
+                        db_manager.insert_data("branch_structural_unit", "branch_office_id, structural_unit_id", f"'{id_branch}', '{structural_unit_id}'")
+
+                CTkMessagebox(title="Успех", message="Структурные подразделения успешно привязаны к филиалу!", icon="check", option_1="Ok")
+
+            except Exception as e:
+                CTkMessagebox(title="Ошибка", message=f"Ошибка при привязке структурного подразделения к филиалу: {str(e)}", icon="cancel")
+                return
         except Exception as e:
-            CTkMessagebox(title="Ошибка", message=f"Ошибка при добавлении структурного подразделения: {str(e)}", icon="cancel")
-            return
-
-        # Получение идентификаторов структурных подразделений и их добавление к филиалу
-        try:
-            for text in list_of_str_units:
-                # Проверяем, привязано ли структурное подразделение к филиалу
-                if not db_manager.check_structural_unit_assigned_to_branch(text, branch_office):
-                    # Если структурное подразделение не привязано к филиалу, добавляем его привязку
-                    structural_unit_id = db_manager.get_data("structural_unit", "id", f"name = '{text}'")[0][0]
-                    db_manager.insert_data("branch_structural_unit", "branch_office_id, structural_unit_id", f"'{id_branch}', '{structural_unit_id}'")
-
-            print("Добавлены новые структурные подразделения к филиалу.")
-        except Exception as e:
-            CTkMessagebox(title="Ошибка", message=f"Ошибка при привязке структурного подразделения к филиалу: {str(e)}", icon="cancel")
+            CTkMessagebox(title="Ошибка", message=f"Ошибка! + {str(e)}", icon="cancel")
             return
 
     def callback_(self, choice):
         assigned_structural_units = db_manager.exec_procedure("GetStructuralUnits", choice)
         assigned_structural_units = [item[0] for item in assigned_structural_units]
-        print(assigned_structural_units)
+
 
         # Удаление всех виджетов с формы
         for widget in self.list_widgets:
@@ -153,9 +158,9 @@ class AioBranchOfficeStructuralUnit(customtkinter.CTkToplevel):
             add_button = customtkinter.CTkButton(master=self.frame_, text="+", command=lambda idx=i + 2: self.add_new_entry(idx))
             add_button.grid(row=len(assigned_structural_units) + 1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
             self.list_widgets.append(add_button)
-            print(self.list_widgets)
+
         else:
-            print("Нет данных для отображения")
+
             CTkMessagebox(title="Ошибка", message="Нет данных для отображения!", icon="warning")
             # Добавление кнопки "+" для возможности добавления данных
             add_button = customtkinter.CTkButton(master=self.frame_, text="+", command=lambda: self.add_new_entry(1))
@@ -164,12 +169,12 @@ class AioBranchOfficeStructuralUnit(customtkinter.CTkToplevel):
 
     def edit_entry(self, idx):
         try:
-            print(idx)
+
             old_value = self.list_widgets_only_entry[idx].get()
 
             dialog = customtkinter.CTkInputDialog(text="Введите новое значение для структурного подразделения:", title="Test")
             new_value = dialog.get_input()  # waits for input
-            print(new_value)
+
 
             # Проверяем, если новое значение не равно None (значение, когда пользователь нажимает Cancel)
             if new_value is not None and new_value.strip():  # Проверка на пустую строку или строку пробелов
@@ -233,7 +238,7 @@ class AioBranchOfficeStructuralUnit(customtkinter.CTkToplevel):
 
         # Create a new row for the entry
         new_row_index = new_row_index
-        print(new_row_index)
+
         label = customtkinter.CTkLabel(master=self.frame_, text=f"Структурное подразделение {new_row_index}", font=("Arial", 12))
         label.grid(row=new_row_index, column=0, padx=10, pady=10, sticky="w")
         self.list_widgets.append(label)
